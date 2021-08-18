e9e8f059f8bf78eca4a29502ad46ffb6f362ca06
lê văn sơn <levanson180200@gmail.com>
2021-08-18 22:13:32 +0700

    create skils list

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,9 +16,9 @@ if(navClose){
     })
 }
 
-//****************  REMOVE MENU MABILE  ********************//
+//****************  REMOVE MENU   ********************//
 const navLink = document.querySelectorAll('.nav__link');
-    // when you click on each nav__link, we remove the show-menu classs
+    // when you click on each nav__link, we remove the show-menu class
 function linkAction(){ 
     navMenu.classList.remove('show-menu');
 }
@@ -26,11 +26,28 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 
 
 
+/********************************************************************************/ 
+/********************************** SKILLS LIST *********************************/ 
+const   skillsContent = document.getElementsByClassName('skill__content');
+        skillHeader = document.querySelectorAll('skill__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className;
+
+    for(var i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skill__content skill__close';
+    }
+    if(itemClass === 'skill__content skill__close'){
+        this.parentNode.className = 'skill__content skill__open';
+    } 
+}
+skillHeader.forEach((e) => {
+    e.addEventListener('click', toggleSkills);
+});
 
 
 
-
-
+~
 
 
 

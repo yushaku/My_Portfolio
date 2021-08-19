379f7d59692ee9fb00e1a3e544d146958da1b392
lê văn sơn <levanson180200@gmail.com>
2021-08-19 15:11:03 +0700

    create qualification

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,10 +47,27 @@ skillHeader.forEach((e) => {
     e.addEventListener('click', toggleSkills)
 });
 
+/**************************************************************************************/
+/*********************************  QUALIFICATION TAB  ********************************/  
+const   tabs = document.querySelectorAll('[data-target]'),
+        tabContents = document.querySelectorAll('[data-content]');
 
+tabs.forEach(tab => {
 
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
 
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active');
+        })
+        target.classList.add('qualification__active');
 
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active');
+        })
+        tab.classList.add('qualification__active');
+    })
+});
 
 
 

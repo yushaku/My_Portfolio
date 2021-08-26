ddc3da47d8d58ab3e2aed1cd2a90b0dfedb413a6
lê văn sơn <levanson180200@gmail.com>
2021-08-26 21:21:28 +0700

    Portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,8 +69,22 @@ tabs.forEach(tab => {
     })
 });
 
+/**************************  PORTFOLIO SWIPER  ******************************/ 
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    mousewheel: true,
+    keyboard: true,
 
-
+});
 
 
 
